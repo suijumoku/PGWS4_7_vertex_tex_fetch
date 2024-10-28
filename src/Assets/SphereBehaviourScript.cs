fe383278374c68525a24251cce6e6686b1b36425
suijumoku <annryuuki@gmail.com>
2024-10-28 21:07:43 +0900

    色を変えた

---
+++ src/Assets/SphereBehaviourScript.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class SphereBehaviourScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition = new Vector3(3f * Mathf.Sin(1.0f * Time.time), 0.0f, 1.0f);
+    }
+}
